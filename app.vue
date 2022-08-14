afe2fd79170189a58514ee2956eb158dfc763fbb
Rail Shakurov <shakurov-98@mail.ru>
2022-08-14 04:38:50 +0300

    +add

--- app.vue
+++ app.vue
@@ -4,4 +4,5 @@
 
 <style>
   @import "@/modules/style";
+  @import "@/modules/grid";
 </style>
---
+++ pages/test/grid.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <div class="px-1">
+      <div class="row mx-n1 mb-1">
+        <div class="col px-1">
+          <div class="bg-mediumseagreen p-1">
+            <div class="bg-black h-3"></div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="row mx-n1 mb-1">
+        <div class="col px-1">
+          <div class="bg-mediumseagreen p-1">
+            <div class="bg-black h-3" style="width: calc(100% + 5px);"></div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="row mx-n1 mb-1">
+        <div class="col px-1">
+          <div class="bg-mediumseagreen p-1">
+            <div class="bg-black h-3" style="width: 110%;"></div>
+          </div>
+        </div>
+        <div class="col-5 px-1">
+          <div class="bg-mediumseagreen p-1">
+            <div class="bg-black h-3"></div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="row mx-n1 mb-1">
+        <div class="col-5 px-1">
+          <div class="bg-mediumseagreen p-1">
+            <div class="bg-black h-3"></div>
+          </div>
+        </div>
+        <div class="col-5 px-1">
+          <div class="bg-mediumseagreen p-1">
+            <div class="bg-black h-3"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row mx-n1 mb-1">
+        <div class="col-1 px-1">
+          <div class="bg-mediumseagreen p-1">
+            <div class="bg-black h-3"></div>
+          </div>
+        </div>
+        <div class="col-5 px-1">
+          <div class="bg-mediumseagreen p-1">
+            <div class="bg-black h-3"></div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="row mx-n1 mb-1">
+        <div class="col-1 px-1">
+          <div class="bg-mediumseagreen p-1">
+            <div class="bg-black h-3" style="width: 110%;"></div>
+          </div>
+        </div>
+        <div class="col-5 px-1">
+          <div class="bg-mediumseagreen p-1">
+            <div class="bg-black h-3"></div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="row mx-n1 mb-1">
+        <div class="col-1 px-1">
+          <div class="bg-mediumseagreen p-1">
+            <div class="bg-black h-3" style="width: 110%;"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+  .bg-mediumseagreen {
+    background-color: mediumseagreen;
+  }
+  .bg-black {
+    background-color: black;
+  }
+  .mx-n1 {
+    margin-left: -5px;
+    margin-right: -5px;
+  }
+  .mb-1 {
+    margin-bottom: 5px;
+  }
+  .p-1 {
+    padding: 5px;
+  }
+  .px-1 {
+    padding-left: 5px;
+    padding-right: 5px;
+  }
+  .h-3 {
+    height: 15px;
+  }
+</style>
