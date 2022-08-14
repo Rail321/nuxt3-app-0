5abcaa0f91fb4ce0918f569af62b082581564304
Rail Shakurov <shakurov-98@mail.ru>
2022-08-14 04:47:47 +0300

    +add

---
+++ pages/test/navigation.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="bg-ghostwhite">
+    <div class="row">
+      <div class="col-1">
+        <div class="h-100 p-2 bg-midnightblue">
+          <div class="min-vh-100 top-0 position-sticky">
+            <div class="h-4 mb-2 bg-white"></div>
+            <div class="h-4 mb-2 bg-white"></div>
+            <div class="h-4 mb-2 bg-white"></div>
+            <div class="h-4 mb-2 bg-white"></div>
+            <div class="h-4 mb-2 bg-white"></div>
+            <div class="h-4 mb-2 bg-white"></div>
+            <div class="h-4 mb-2 bg-white"></div>
+            <div class="h-4 mb-2 bg-white"></div>
+            <div class="h-4 mb-2 bg-white"></div>
+            <div class="h-4 mb-2 bg-white"></div>
+            <div class="h-4 mb-2 bg-white"></div>
+            <div class="h-4 mb-2 bg-white"></div>
+            <div class="h-4 mb-2 bg-white"></div>
+            <div class="h-4 mb-2 bg-white"></div>
+            <div class="h-4 mb-2 bg-white"></div>
+            <div class="h-4 mb-2 bg-white"></div>
+            <div class="h-4 mb-2 bg-white"></div>
+            <div class="h-4 bg-white"></div>
+          </div>
+        </div>
+      </div>
+      <div class="col-5">
+        <div class="min-vh-100 p-2">
+          <div class="w-25 h-4 mb-2 bg-black"></div>
+          <div class="h-4 mb-2 bg-black"></div>
+          <div class="h-4 mb-2 bg-black"></div>
+          <div class="h-4 mb-4 bg-black"></div>
+          
+          <div class="w-25 h-4 mb-2 bg-black"></div>
+          <div class="h-4 mb-4 bg-black"></div>
+          
+          <div class="w-25 h-4 mb-2 bg-black"></div>
+          <div class="h-4 mb-2 bg-black"></div>
+          <div class="h-4 mb-2 bg-black"></div>
+          <div class="h-4 mb-2 bg-black"></div>
+          <div class="h-4 mb-2 bg-black"></div>
+          <div class="h-4 mb-2 bg-black"></div>
+          <div class="h-4 mb-4 bg-black"></div>
+          
+          <div class="w-25 h-4 mb-2 bg-black"></div>
+          <div class="h-4 mb-2 bg-black"></div>
+          <div class="h-4 mb-2 bg-black"></div>
+          <div class="h-4 mb-4 bg-black"></div>
+          
+          <div class="w-25 h-4 mb-2 bg-black"></div>
+          <div class="h-4 mb-2 bg-black"></div>
+          <div class="h-4 mb-2 bg-black"></div>
+          <div class="h-4 mb-2 bg-black"></div>
+          <div class="h-4 mb-2 bg-black"></div>
+          <div class="h-4 mb-2 bg-black"></div>
+          <div class="h-4 mb-4 bg-black"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .min-vh-100 {
+    min-height: 100vh;
+  }
+  .bg-ghostwhite {
+    background-color: ghostwhite;
+  }
+  .bg-midnightblue {
+    background-color: midnightblue;
+  }
+  .bg-white {
+    background-color: white;
+  }
+  .bg-black {
+    background-color: black;
+  }
+
+  .mb-2 {
+    margin-bottom: 10px;
+  }
+  .mb-4 {
+    margin-bottom: 20px;
+  }
+
+  .p-2 {
+    padding: 10px;
+  }
+
+  .w-25 {
+    width: 25%;
+  }
+
+  .h-4 {
+    height: 20px;
+  }
+  .h-100 {
+    height: 100%;
+  }
+
+  .top-0 {
+    top: 0;
+  }
+
+  .position-sticky {
+    position: sticky;
+  }
+</style>
